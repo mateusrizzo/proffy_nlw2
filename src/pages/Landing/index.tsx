891007723497f3b238cc789c3e86ae0680a0d007
Mateus Gonçalves Rizzo <mateusrizzo13@gmail.com>
2020-08-03 18:24:44 -0300

    navigation routes added

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom'
+
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 
@@ -19,14 +21,14 @@ function Landing(){
                 </div>
                 <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="/give-class" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas"/>
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
                 <span className="total-connections">
                     Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
